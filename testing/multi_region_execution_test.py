448c7b0c8eed82446eae130207d5a9236474e3e7
Jarrett Andrulis <jandruli@amazon.com>
2021-08-18 20:01:20 -0500

    Fixed merge comflict

--- testing/multi_region_execution_test.py
+++ testing/multi_region_execution_test.py
@@ -11,6 +11,12 @@ import boto3
 test_file_name = "test-region.yaml"
 # create region file
 test_file = """
+default:
+  - ap-east-1
+  - us-west-2
+test-commercial:
+  - ap-east-1
+  - us-west-1
 """
 
 with open(test_file_name, "w+") as f:
